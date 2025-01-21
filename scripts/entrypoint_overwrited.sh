430fa357eb06fd2641780b165106fd9ac6203308
cloudred <cloudred442@gmail.com>
2025-01-21 17:39:28 +0800

    newest commit

--- scripts/entrypoint_overwrited.sh
+++ scripts/entrypoint_overwrited.sh
@@ -14,5 +14,35 @@ if [ -f /run/secrets/directUrl ]; then
   export DIRECT_URL=$(cat /run/secrets/directUrl)
 fi
 
+
+if [ -f /run/secrets/redisUrl ]; then
+  export UPSTASH_REDIS_REST_URL=$(cat /run/secrets/redisUrl)
+fi
+
+
+if [ -f /run/secrets/redisToken ]; then
+  export UPSTASH_REDIS_REST_TOKEN=$(cat /run/secrets/redisToken)
+fi
+
+
+if [ -f /run/secrets/resendKey ]; then
+  export RESEND_API_KEY=$(cat /run/secrets/resendKey)
+fi
+
+if [ -f /run/secrets/moviderKey ]; then
+  export MOVIDER_API_KEY=$(cat /run/secrets/moviderKey)
+fi
+
+
+if [ -f /run/secrets/moviderSecret ]; then
+  export MOVIDER_API_SECRET=$(cat /run/secrets/moviderSecret)
+fi
+
+
+
+
+
+
+
 # Start the application
 exec "$@"
