37dd9f53ca3b57b7de4f6e820e4568119a0beb2b
cloudred <cloudred442@gmail.com>
2025-01-22 19:24:40 +0800

    newest commit

--- components/LoginPageSecured/page.tsx
+++ components/LoginPageSecured/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { handleSigninAdmin } from "@/app/actions/auth/authAction";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
   InputOTP,
@@ -14,7 +15,7 @@ import { createClientSide } from "@/utils/supabase/client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Image from "next/image";
 import { useState } from "react";
-import { FieldErrors, useForm } from "react-hook-form";
+import { FieldErrors, SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
 import { Card } from "../ui/card";
 import { Label } from "../ui/label";
@@ -161,14 +162,21 @@ const LoginPageSecured = () => {
         <Image src="/app-logo.png" alt="logo" width={400} height={250} />
       </div>
       <Card className="w-full max-w-lg z-40 relative p-6">
-        {step === "login" && (
-          <>
+        {step === "login" ? (
+          <form
+            className="flex flex-col items-center gap-6"
+            onSubmit={handleSubmit(
+              handleSignIn as SubmitHandler<
+                { userName: string } | { otp: string }
+              >
+            )}
+          >
             <div className="w-full text-center">
               <Label htmlFor="username">Username</Label>
               <Input
                 variant="non-card"
                 id="username"
-                {...register("userName" as const)}
+                {...register("userName")}
               />
               {(errors as FieldErrors<LoginFormValues>).userName && (
                 <p className="text-sm text-primaryRed">
@@ -182,7 +190,7 @@ const LoginPageSecured = () => {
               <PasswordInput
                 variant="non-card"
                 id="password"
-                {...register("password" as const)}
+                {...register("password")}
               />
               {(errors as FieldErrors<LoginFormValues>).password && (
                 <p className="text-sm text-primaryRed">
@@ -190,35 +198,58 @@ const LoginPageSecured = () => {
                 </p>
               )}
             </div>
-          </>
-        )}
-
-        {step === "verify" && (
-          <div className="w-full justify-center items-center flex flex-col gap-2">
-            <Label htmlFor="otp">Enter OTP</Label>
-            <InputOTP
-              maxLength={6}
-              value={watch("otp")}
-              onChange={(value) => setValue("otp", value)}
+            <Button
+              variant="card"
+              className="w-72 rounded-md"
+              disabled={isSubmitting || isLoading}
+              type="submit"
             >
-              <InputOTPGroup>
-                <InputOTPSlot index={0} />
-                <InputOTPSlot index={1} />
-                <InputOTPSlot index={2} />
-              </InputOTPGroup>
-              <InputOTPSeparator />
-              <InputOTPGroup>
-                <InputOTPSlot index={3} />
-                <InputOTPSlot index={4} />
-                <InputOTPSlot index={5} />
-              </InputOTPGroup>
-            </InputOTP>
-            {(errors as FieldErrors<OtpFormValues>).otp && (
-              <p className="text-sm text-primaryRed">
-                {(errors as FieldErrors<OtpFormValues>).otp?.message}
-              </p>
+              {isSubmitting || isLoading ? "Sending OTP..." : "Login"}
+            </Button>
+          </form>
+        ) : (
+          <form
+            className="flex flex-col w-full items-center gap-6"
+            onSubmit={handleSubmit(
+              handleVerifyOtp as SubmitHandler<
+                { userName: string } | { otp: string }
+              >
             )}
-          </div>
+          >
+            <div className="w-full justify-center items-center flex flex-col gap-2">
+              <Label htmlFor="otp">Enter OTP</Label>
+              <InputOTP
+                maxLength={6}
+                value={watch("otp")}
+                onChange={(value) => setValue("otp", value)}
+              >
+                <InputOTPGroup>
+                  <InputOTPSlot index={0} />
+                  <InputOTPSlot index={1} />
+                  <InputOTPSlot index={2} />
+                </InputOTPGroup>
+                <InputOTPSeparator />
+                <InputOTPGroup>
+                  <InputOTPSlot index={3} />
+                  <InputOTPSlot index={4} />
+                  <InputOTPSlot index={5} />
+                </InputOTPGroup>
+              </InputOTP>
+              {(errors as FieldErrors<OtpFormValues>).otp && (
+                <p className="text-sm text-primaryRed">
+                  {(errors as FieldErrors<OtpFormValues>).otp?.message}
+                </p>
+              )}
+            </div>
+            <Button
+              variant="card"
+              className="w-72 rounded-md"
+              disabled={isSubmitting || isLoading}
+              type="submit"
+            >
+              {isSubmitting || isLoading ? "Verifying OTP..." : "Verify"}
+            </Button>
+          </form>
         )}
       </Card>
     </div>
