7c5f989245d4421c29bf64d352c0ca2859608b3e
cloudred <cloudred442@gmail.com>
2025-01-29 01:59:41 +0800

    fix health check

--- app/api/health/route.ts
+++ app/api/health/route.ts
@@ -1,19 +1,5 @@
-import { rateLimit } from "@/utils/redis/redis";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: Request) {
-  const ip =
-    request.headers.get("x-forwarded-for")?.split(",")[0].trim() ||
-    request.headers.get("cf-connecting-ip") ||
-    "unknown";
-
-  const isAllowed = await rateLimit(`rate-limit:${ip}`, 5, 60);
-
-  if (!isAllowed) {
-    return sendErrorResponse("Too many requests. Please try again later.", 429);
-  }
+export async function GET(request: NextRequest) {
   return NextResponse.json({ status: "OK" }, { status: 200 });
-}
-function sendErrorResponse(arg0: string, arg1: number) {
-  throw new Error("Function not implemented.");
 }
